b1b815ebe1e32b78644d567c0e27a9a3d9b78cb1
airynwn <laura.alcon@iesdonana.org>
2024-11-03 20:53:22 +0100

    first commit (test)

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+if (!app.isPackaged) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
