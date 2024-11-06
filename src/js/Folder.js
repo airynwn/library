9449f55fc852631ce661143edbc1a14eb0e2e6d5
airynwn <laura.alcon@iesdonana.org>
2024-11-06 21:53:32 +0100

    folder icon preview

--- src/js/Folder.js
+++ src/js/Folder.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function Folder({id, title}) {
     return (
-        <div>[ ] {id}.- {title}</div>
+        <Container>
+            <Row>
+                <Col className=" d-flex flex-column align-items-center">
+                    <img src={'src/svg/folder.svg'} className="icon" />
+                    <span>{title}</span>
+                </Col>
+            </Row>
+        </Container>
     );
 }
